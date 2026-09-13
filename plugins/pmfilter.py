c541a2d2fb8fcea1a3cb26c919a7c4302ad562c0
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-09-13 19:25:39 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -876,8 +876,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer("ᴛʜɪs ɪs ᴘᴀɢᴇs ʙᴜᴛᴛᴏɴ 😅")
     
     elif query.data.startswith("sendfiles"):
-    ident, key = query.data.split("#")
-    settings = await get_settings(query.message.chat.id)
+       ident, key = query.data.split("#")
+            settings = await get_settings(query.message.chat.id)
 
     try:
         original_url = (
